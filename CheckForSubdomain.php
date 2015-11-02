5b246f5b9af4e752e666f11b28c7a394c0ead94c
GingerSquirrel <chocolatey_squirrel@hotmail.com>
2015-11-02 16:05:19 +0000

    Complete change of code

--- CheckForSubdomain.php
+++ CheckForSubdomain.php
@@ -1,23 +1,26 @@
 <?php
 checkSubdomain("http://google.com"); //Put domain name in here or $_SERVER['HTTP_HOST'] 
 
+
 function checkSubdomain($url){
-	$dotcount = substr_count($url,"."); //Counts the dots in the URL
-	$urlParts = explode('.', $url); // Breaks down the URL
-	if (($urlParts[0] == "www" OR "http://www" OR "https://www")) { //Checks the first part of the URL
+	$dotcount = substr_count($url,".");
+	$urlParts = explode('.', $url);
+	if ($urlParts[0] =="www" || $urlParts[0] == "http://www" || $urlParts[0] == "https://www") {
 		if ($dotcount >= 3) {
-			echo "Subdomain";
+			echo "Subdomain"; // http://www.subdomain.domain.com
 		}else{
-			if ($dotcount >= 2) {
-				echo "Subdomain";
-			}else{
-				echo "Domain";
-			}
+			echo "Domain"; // www.domain.com
 		}
 	}else{
-		echo "Something has gone wrong...";
+		if ($dotcount >= 2) {
+			echo "Subdomain"; // subdomain.domain.com
+
+		}else{
+			echo "Domain"; //domain.com
+		}
 	}
 }
+
 
 
 ?>
